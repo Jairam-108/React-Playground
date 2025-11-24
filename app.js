29096c98067473ffff964555a83cab2ec5a6c34b
Jairam-108 <srinu.mdl7@gmail.com>
2025-11-24 20:36:12 +0530

    lession 2 commits

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 const parent = React.createElement('div', {
     id: 'parent'
 }, React.createElement('div', {id: 'child'},  [
-    React.createElement('h1', {id: 'head-txt'},  'Hello wold from h1 react'),
-    React.createElement('h2', {id: 'h2-txt'},  'Hello wold from h2 react')
+    React.createElement('h1', {id: 'head-txt', key: 'head-1'},  'Hello wold from h1 react'),
+    React.createElement('h2', {id: 'h2-txt', key: 'head-2'},  'Hello wold from h2 react')
 ]));
 console.log('parent', parent);
 const root = ReactDOM.createRoot(document.getElementById('root'));
